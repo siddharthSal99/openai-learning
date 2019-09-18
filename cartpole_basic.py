a569ba67fac1a521368d3bb4b4a123329ea3525d
SiddharthSalunkhe <siddharth.salunkhe11@gmail.com>
2019-09-18 18:18:02 -0400

    making progress with dqn agent

--- cartpole_basic.py
+++ cartpole_basic.py
@@ -1,12 +1,36 @@
 import gym
 import custom_env
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+# https://github.com/gsurma/cartpole/blob/master/cartpole.py
+
+MAX_EPS = 1000
+
+class DQNAgent:
+	def __init__(self,obs_space,act_space):
+
+	def add_to_buffer(self, stateSet):
+
+	def chooseAction(self):
+
+	def learn(self):
+
+
+
+
 env = gym.make("sid_cartpole-v0")
 observation = env.reset()
-for _ in range(1000):
+for _ in range(MAX_EPS):
 	env.render()
 	action = env.action_space.sample() # your agent here (this takes random actions)
 	observation, reward, done, info = env.step(action)
-
+	print observation, reward, done, info
 	if done:
 		observation = env.reset()
 env.close()
+
+
